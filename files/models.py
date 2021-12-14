4827a8b0b570a168d168e0aa19f5a602fb693caf
rahim <rahim.aghareb@gmail.com>
2021-12-14 15:36:20 +0330

    files app edited

--- files/models.py
+++ files/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    TYPE_CHOICES =[
+        ('.excel','xls'),
+        ('.pdf','pdf'),
+    ]
+    name = models.CharField(max_length=100)
+    file = models.FileField(upload_to='media/files/upload/')
+    type = models.CharField(max_length=8,
+                            choices=TYPE_CHOICES,
+                            default='xls')
