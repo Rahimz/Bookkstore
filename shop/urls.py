97c6e4678bb4e57fb37e23cde63332c9c72ef9b1
rahim <rahim.aghareb@gmail.com>
2021-12-16 20:00:11 +0330

    product detail link and home page product added

--- shop/urls.py
+++ shop/urls.py
@@ -2,9 +2,10 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
+app_name = 'shop'
 
 urlpatterns = [
-    path('product/123/', views.product_detail, name='product'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
     path('', views.temp_home, name='temp_home'),
     # path('', views.home, name='home'),
     path('home/', views.home, name='home'),
--- shop/views.py
+++ shop/views.py
@@ -1,19 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
 
 
 def home(request):
+    products = Product.objects.all()
     return render(
         request,
         'home.html',
-        {}
+        {
+        'products':products,
+        }
     )
 
 
-def product_detail(request):
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
     return render(
         request,
         'shop/product_detail.html',
-        {}
+        {
+        'product':product,
+        }
     )
 
 def temp_home(request):
