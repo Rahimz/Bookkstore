bf247c18f9a5863add93288d118be87ad7650c46
rahim <rahim.aghareb@gmail.com>
2021-12-18 14:20:45 +0330

    ImportSession object added to files app

--- files/models.py
+++ files/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import uuid
+from django.conf import settings
 
 
 class File(models.Model):
@@ -6,9 +8,9 @@ class File(models.Model):
     This class is created for manual file upload to database.
     the file stores in media and help the admin to import it to database.
     """
-    TYPE_CHOICES =[
-        ('.excel','xls'),
-        ('.pdf','pdf'),
+    TYPE_CHOICES = [
+        ('.excel', 'xls'),
+        ('.pdf', 'pdf'),
     ]
     name = models.CharField(
         max_length=100
@@ -26,3 +28,20 @@ class File(models.Model):
         choices=TYPE_CHOICES,
         default='xls'
     )
+
+
+class ImportSession(models.Model):
+    """
+    This class keep the import session histroy to keep the record
+    and make the posibility of rollback.
+    """
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created = models.DateTimeField(auto_now_add=True)
