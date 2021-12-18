2cdfcedbba9eb088f4b8d68c261ecbe702d5f0d3
rahim <rahim.aghareb@gmail.com>
2021-12-18 17:53:43 +0330

    robots.txt added to the project

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
+
 from . import views
 
 app_name = 'shop'
@@ -9,4 +11,5 @@ urlpatterns = [
     path('', views.temp_home, name='temp_home'),
     # path('', views.home, name='home'),
     path('home/', views.home, name='home'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
