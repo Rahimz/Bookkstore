b14d6d27f47335384d19ea51b54dff75fe454f06
rahim <rahim.aghareb@gmail.com>
2021-12-27 16:02:19 +0330

    staff page templates added

--- staff/urls.py
+++ staff/urls.py
@@ -8,6 +8,7 @@ app_name = 'staff'
 
 urlpatterns = [
     path('orders/', views.orders, name='order_list'),
+    path('orders/create/', views.order_create, name='order_create'),
     path('purchases/', views.purchases, name='purchase_list'),
     path('warehouse/', views.warehouse, name='warehouse'),
     path('products/', views.products, name='products'),
