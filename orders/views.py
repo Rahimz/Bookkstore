413de460f3c40aa0de2c9fa9f01047e42e7cde8a
rahim <rahim.aghareb@gmail.com>
2022-01-01 15:57:20 +0330

    Order checkout added

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import OrderLine
+from .forms import OrderCreateForm
+from cart.cart import Cart
+
+
+@login_required
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.user = request.user
+            # if cart.coupon:
+                # order.coupon = cart.coupon
+                # order.discount = cart.coupon.discount
+            order.save()
+            for item in cart:
+                OrderLine.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            # clear the cart
+            cart.clear()
+            # launch asynchronous task
+            # order_created.delay(order.id)
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
+            # return redirect(reverse('zarinpal:request'))
+            return render(request,
+                          'orders/created.html',
+                          {'order': order})
+    else:
+        # try:
+        #     address = Address.objects.get(user=request.user, fav_address=True)
+        #     if address:
+        #         data_initial = {
+        #         'first_name' : address.first_name,
+        #         'last_name' : address.last_name,
+        #         'phone' : address.phone,
+        #         'address' : address.address,
+        #         'postal_code' : address.postal_code,
+        #         'city' : address.city}
+        #         form = OrderCreateForm(data_initial)
+        # except:
+        #     form = OrderCreateForm()
+        form = OrderCreateForm()
+    return render(request,
+                  'orders/create.html',
+                  {'cart': cart, 'form': form})
