0bc842f09ff3d3e49d03d0130cee82c3f7eb53c2
rahim <rahim.aghareb@gmail.com>
2022-01-07 12:05:12 +0330

    crispy form added to the project

--- config/settings/base.py
+++ config/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django_countries',
     'debug_toolbar',
     'rosetta',
+    'crispy_forms',
 
     # Local
     'account.apps.AccountConfig',
@@ -197,3 +198,6 @@ EMAIL_HOST = email_host
 EMAIL_HOST_USER = email_host_user
 EMAIL_HOST_PASSWORD = email_host_password
 DEFAULT_FROM_EMAIL = 'noreply@ketabedamavand.com'
+
+#  Django drispy form settings
+CRISPY_TEMPLATE_PACK = 'uni_form'
