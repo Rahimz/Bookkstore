b0ca4b90e1c4f956d144cb04a15406e921bf560d
rahim <rahim.aghareb@gmail.com>
2022-01-11 16:07:02 +0330

    order list by time period updated

--- staff/urls.py
+++ staff/urls.py
@@ -8,10 +8,10 @@ app_name = 'staff'
 
 urlpatterns = [
     path('sales/', views.sales, name='sales_list'),
-    path('orders/', views.orders, name='order_list'),
     path('orders/<int:pk>/', views.order_detail_for_admin, name='order_detail_for_admin'),
     path('orders/drafts/', views.draft_orders, name='draft_orders'),
     path('orders/create/', views.order_create, name='order_create'),
+    path('orders/<str:period>/', views.orders, name='order_list'),
     path('invoice/create/', views.invoice_create, name='invoice_create'),
     path('invoice/create/<int:order_id>/', views.invoice_create, name='invoice_create'),
     path('invoice/checkout/<int:order_id>/', views.invoice_checkout, name='invoice_checkout'),
