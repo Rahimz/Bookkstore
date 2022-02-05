ceaddcbacd01a814a9cd4f42918c6fbccaa75371
rahim <rahim.aghareb@gmail.com>
2022-02-05 15:47:33 +0330

    improvement - PDF to A4 size added

--- tools/urls.py
+++ tools/urls.py
@@ -5,7 +5,8 @@ app_name = 'tools'
 
 urlpatterns = [
     path('pdf/<int:order_id>/', views.make_invoice_pdf, name="make_invoice_pdf"),
+    path('pdf/a4/<int:order_id>/', views.make_invoice_pdf_a4, name="make_invoice_pdf_a4"),
 
     path('order-export/', views.order_export_excel, name="order_export_excel"),
-    path('draft-order-export/', views.draft_order_export_excel, name="draft_order_export_excel"),    
+    path('draft-order-export/', views.draft_order_export_excel, name="draft_order_export_excel"),
 ]
