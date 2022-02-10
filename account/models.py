2c86118cc61ef7e44767ec4450b96e8094d6ff2a
rahim <rahim.aghareb@gmail.com>
2022-02-10 09:19:49 +0330

    bug - excel export bug is fixed

--- account/models.py
+++ account/models.py
@@ -48,13 +48,19 @@ class Address(models.Model):
         ordering = ("pk",)
 
     def __str__(self):
-        return self.name
+        try:
+            return self.name
+        except:
+            return ""
 
     def get_absolute_url(self):
         return reverse('address_detail',
                        args=[self.id])
     def get_full_address(self):
-        return f"{self.country.name} - {self.city} - {self.street_address_1}, {self.street_address_2} - {self.house_number} - {self.house_unit} - {self.postal_code}"
+        try:
+            return f"{self.country.name} - {self.city} - {self.street_address_1}, {self.street_address_2} - {self.house_number} - {self.house_unit} - {self.postal_code}"
+        except:
+            return ""
 
 
 class CustomUser(AbstractUser):
