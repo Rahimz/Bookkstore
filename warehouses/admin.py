723a2870440ffa87f3e7e8092db6bb98adeb5a20
rahim <rahim.aghareb@gmail.com>
2022-02-18 21:02:48 +0330

    improvement - Refund model added to warehouse app

--- warehouses/admin.py
+++ warehouses/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Warehouse
+from .models import Warehouse, Refund
 
 
 @admin.register(Warehouse)
@@ -9,3 +9,17 @@ class WarehouseAdmin(admin.ModelAdmin):
         'name'
     ]
     # search_fields = ['order', 'product', 'variation']
+
+
+@admin.register(Refund)
+# class ProductAdmin(admin.ModelAdmin):
+class RefundAdmin(admin.ModelAdmin):
+    list_display = [
+        'product', 'order', 'purchase',
+        'quantity', 'price', 'from_client',
+         'to_vendor']
+    # list_filter = ['available', 'stock' ]
+    # list_editable = ['price', 'available', 'stock',]
+    # prepopulated_fields = {'slug': ('name',)}
+    # ordering = ['name', 'created', 'updated']
+    # search_fields = ['name', 'author', 'isbn']
--- warehouses/models.py
+++ warehouses/models.py
@@ -1,10 +1,14 @@
 from django.db import models
-# from django.conf import settings
+from django.conf import settings
 # import uuid
 from django.utils.translation import gettext_lazy as _
 # from django.core.validators import MinValueValidator, MaxValueValidator
 # from decimal import Decimal
 from datetime import datetime
+
+from products.models import Product
+from orders.models import Order, Purchase
+from account.models import CustomUser, Vendor
 
 
 class Warehouse(models.Model):
@@ -20,3 +24,74 @@ class Warehouse(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class Refund(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    purchase = models.ForeignKey(
+        Purchase,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=0,
+        default=0
+    )
+    discount = models.DecimalField(
+        max_digits=10,
+        decimal_places=0,
+        default=0
+    )
+    quantity = models.IntegerField(
+        default=0
+    )
+    from_client = models.ForeignKey(
+        CustomUser,
+        on_delete=models.SET_NULL,
+        related_name='refund_client',
+        null=True,
+        blank=True
+    )
+    to_vendor = models.ForeignKey(
+        Vendor,
+        on_delete=models.SET_NULL,
+        related_name='refund_vendor',
+        null=True,
+        blank=True
+    )
+    registrar = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='refund_registrar',
+        blank=True,
+        null=True
+    )
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+    approver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='refund_approver',
+        blank=True,
+        null=True
+    )
+    approved_date = models.DateTimeField(
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return f"{self.order.pk} {self.product.name}"
