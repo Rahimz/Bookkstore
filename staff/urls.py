dfccc1ffb1e0db499030c22279815c881a9ddad1
rahim <rahim.aghareb@gmail.com>
2022-02-19 11:19:43 +0330

    improvement - pay with credit added

--- staff/urls.py
+++ staff/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('invoice/create/line-update/<int:order_id>/<int:orderline_id>/', views.orderline_update, name='orderline_update'),
     path('invoice/checkout/<int:order_id>/', views.invoice_checkout, name='invoice_checkout'),
     path('invoice/checkout/<int:order_id>/<int:client_id>/', views.invoice_checkout, name='invoice_checkout_client'),
+    path('invoice/checkout/credit-pay/<int:order_id>/<int:client_id>/', views.invoice_checkout_credit, name='invoice_checkout_client_credit'),
 
     path('warehouse/', views.warehouse, name='warehouse'),
     path('warehouse/sold-products/', views.sold_products, name='sold_products'),
