a1c7b6a84fa91f3b0147eb7c0a7d483d8fdd7595
rahim <rahim.aghareb@gmail.com>
2022-03-02 11:22:41 +0330

    improvement - export used product with noprice

--- tools/urls.py
+++ tools/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('draft-order-export/', views.draft_order_export_excel, name="draft_order_export_excel"),
 
     path('product-export/', views.product_export_excel, name="product_export_excel"),
+    path('used-product-without-price-export/<str:filter>/', views.product_export_excel, name="used_noprice_export_excel"),
 
     path('qrcode/create/<int:order_id>/<int:payment_id>/', views.qrcode_create, name='qrcode_create'),
 ]
