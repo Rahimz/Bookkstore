6f90d750ad2da6eb4baf7f910c527b7a22fb8a55
rahim <rahim.aghareb@gmail.com>
2022-03-08 13:17:53 +0330

    publisher export created

--- tools/urls.py
+++ tools/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
 
     path('order-export/<str:criteria>/', views.order_export_excel, name="order_export_excel"),
     path('draft-order-export/', views.draft_order_export_excel, name="draft_order_export_excel"),
+    path('publishers-export/', views.export_publisher, name="export_publisher"),
 
     path('product-export/', views.product_export_excel, name="product_export_excel"),
     path('used-product-without-price-export/<str:filter>/', views.product_export_excel, name="used_noprice_export_excel"),
