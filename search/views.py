ca67fb919e06e1dab1641f8392ba3c301231e8ae
rahim <rahim.aghareb@gmail.com>
2022-03-10 16:59:18 +0330

    refactor - product naming with collection added

--- search/views.py
+++ search/views.py
@@ -10,7 +10,7 @@ def ProductSearch(object, query):
     Search an object model base on a search query
     """
     results = object.objects.all().filter(available=True).annotate(
-        search=SearchVector('name', 'author', 'translator', 'publisher', 'isbn', 'isbn_9'),
+        search=SearchVector('name', 'author', 'translator', 'publisher', 'isbn', 'isbn_9', 'collection_name', 'pk'),
     ).filter(search=query)
     return results
 
