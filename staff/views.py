cf02ee444d7210766c8eedfebf8ca26e80e28d85
rahim <rahim.aghareb@gmail.com>
2022-03-26 15:42:49 +0430

    refactor - comment added

--- staff/views.py
+++ staff/views.py
@@ -90,7 +90,9 @@ def orders(request, period='all', channel='all', filter=None):
     # order statics
     orders_statics = orders.aggregate(cost=Sum('payable'), quantity=Sum('quantity'))
 
+    # we make a session variable to send order list to other needed veiw
     request.session['orders'] = list(orders.values_list('id', flat=True))
+
     # pagination
     paginator = Paginator(orders, 20)  # 20 order in each page
     page = request.GET.get('page')
