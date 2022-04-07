d81b19bf3b8ec6c2a970af896ce3c3a1ed1707ee
rahim <rahim.aghareb@gmail.com>
2022-04-07 11:55:32 +0430

    improvement - tickets list created

--- tickets/views.py
+++ tickets/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from .models import Ticket
 
 def tickets_list(request):
+    tickets = Ticket.objects.all().order_by('rank')
+
     return render(
         request,
-        'tickets/tickets_list.html'
+        'tickets/tickets_list.html',
+        {
+            'tickets': tickets,
+        }
     )
