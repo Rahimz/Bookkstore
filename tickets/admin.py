40f20c84002079247e0e4aeb11cafca92d3428d7
rahim <rahim.aghareb@gmail.com>
2022-04-07 11:40:10 +0430

    improvement - ticket model added to ticket app

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Ticket
 
-# Register your models here.
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'name', 'registrar',
+        'is_checked', 'is_solved', 'active'
+    ]
+    list_filter = [
+        'is_checked', 'is_solved', 'active'
+    ]
+    search_fields = [
+        'name', 'description'
+    ]
--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+from datetime import datetime
 
-# Create your models here.
+
+from tools.gregory_to_hijry import hij_strf_date, greg_to_hij_date
+
+
+class Ticket(models.Model):
+    PRIORITY_CHOICES = (
+        ('normal', _('Normal')),
+        ('medium', _('medium')),
+        ('high', _('High')),
+    )
+
+    name = models.CharField(
+        max_length=100,
+    )
+    description = models.TextField()
+    priority = models.CharField(
+        max_length=20,
+        default='normal',
+        choices=PRIORITY_CHOICES
+    )
+    rank = models.IntegerField(
+        blank=True,
+        null=True
+    )
+    file = models.FileField(
+        upload_to='tickets/files',
+        null=True,
+        blank=True
+    )
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated = models.DateTimeField(
+        auto_now=True
+    )
+    registrar = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='tickets_registrar',
+        blank=True,
+        null=True
+    )
+    active = models.BooleanField(
+        default=True
+    )
+    is_checked = models.BooleanField(
+        default=False
+    )
+    is_solved = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_fa_created(self):
+        return hij_strf_date(greg_to_hij_date(self.created.date()), '%-d %B %Y')
